3762f2d4c6684f975dea65799c57e9722caf0986
Android <varun@Varun-PC.(none)>
2013-02-04 13:11:38 +0530

    Bugs Solved

--- CCL/src/in/ccl/adapters/TeamMemberGridAdapter.java
+++ CCL/src/in/ccl/adapters/TeamMemberGridAdapter.java
@@ -24,7 +24,7 @@ public class TeamMemberGridAdapter extends BaseAdapter {
 
 	private Context mContext;
 
-	private ArrayList <TeamMember> gridItemsList;
+	private ArrayList<TeamMember> gridItemsList;
 
 	private LayoutInflater inflater;
 
@@ -33,79 +33,136 @@ public class TeamMemberGridAdapter extends BaseAdapter {
 	// A Drawable for a grid cell that's empty
 	private Drawable mEmptyDrawable;
 
-	public TeamMemberGridAdapter (Context context, ArrayList <TeamMember> listOfItems, String from) {
+	public TeamMemberGridAdapter(Context context,
+			ArrayList<TeamMember> listOfItems, String from) {
 		mContext = context;
 		isFrom = from;
 		gridItemsList = listOfItems;
-		inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		mEmptyDrawable = context.getResources().getDrawable(R.drawable.imagenotqueued);
-		Display display = ((Activity) context).getWindowManager().getDefaultDisplay();
-		int height = display.getHeight();
-		reqImageHeight = (int) (((float) 15 / 100) * (height - 50));
+		inflater = (LayoutInflater) mContext
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		mEmptyDrawable = context.getResources().getDrawable(
+				R.drawable.imagenotqueued);
+		if (isFrom.equals("team_members")) {
+
+			Display display = ((Activity) context).getWindowManager()
+					.getDefaultDisplay();
+			int height = display.getHeight();
+			reqImageHeight = (int) (((float) 15 / 100) * (height - 50));
+		} else {
+			Display display = ((Activity) context).getWindowManager()
+					.getDefaultDisplay();
+			int height = display.getHeight();
+			reqImageHeight = (int) (((float) 14 / 100) * (height - 50));
+		}
 	}
 
 	@Override
-	public int getCount () {
+	public int getCount() {
 
 		return gridItemsList.size();
 	}
 
 	@Override
-	public Object getItem (int position) {
+	public Object getItem(int position) {
 
 		return gridItemsList.get(position);
 	}
 
 	@Override
-	public long getItemId (int arg0) {
+	public long getItemId(int arg0) {
 
 		return 0;
 	}
 
 	@Override
-	public View getView (int position, View convertView, ViewGroup parent) {
+	public View getView(int position, View convertView, ViewGroup parent) {
 		ViewHolder mViewHolder;
 		if (convertView == null) {
-			convertView = inflater.inflate(R.layout.team_item, null);
-			mViewHolder = new ViewHolder();
-			// mViewHolder.image = (ImageView) convertView.findViewById(R.id.image);
-			mViewHolder.imgTeamMember = (PhotoView) convertView.findViewById(R.id.img_team_member);
-			mViewHolder.txtPersonName = (TextView) convertView.findViewById(R.id.txt_person_name);
-			mViewHolder.txtRole = (TextView) convertView.findViewById(R.id.txt_role);
-			mViewHolder.errorTxt = (TextView) convertView.findViewById(R.id.error_title);
-			//Util.setTextFont((Activity) mContext, mViewHolder.txtRole);
-			//Util.setTextFont((Activity) mContext, mViewHolder.txtPersonName);
+			if (isFrom.equals("team_members")) {
+				convertView = inflater.inflate(R.layout.team_item, null);
+				mViewHolder = new ViewHolder();
+				// mViewHolder.image = (ImageView)
+				// convertView.findViewById(R.id.image);
+				mViewHolder.imgTeamMember = (PhotoView) convertView
+						.findViewById(R.id.img_team_member);
+				mViewHolder.txtPersonName = (TextView) convertView
+						.findViewById(R.id.txt_person_name);
+				mViewHolder.txtRole = (TextView) convertView
+						.findViewById(R.id.txt_role);
+				mViewHolder.errorTxt = (TextView) convertView
+						.findViewById(R.id.error_title);
+				// Util.setTextFont((Activity) mContext, mViewHolder.txtRole);
+				// Util.setTextFont((Activity) mContext,
+				// mViewHolder.txtPersonName);
 
-			mViewHolder.txtPersonName.setText(gridItemsList.get(position).getPersonName());
-			String role = gridItemsList.get(position).getRole().trim().toLowerCase().replaceAll("\\s+","");
-			if(role.equals(mContext.getResources().getString(R.string.no_role)) || role.equals(mContext.getResources().getString(R.string.ambassadors).toLowerCase())){
-				mViewHolder.txtRole.setText("");
+				mViewHolder.txtPersonName.setText(gridItemsList.get(position)
+						.getPersonName());
+				String role = gridItemsList.get(position).getRole().trim()
+						.toLowerCase().replaceAll("\\s+", "");
+				if (role.equals(mContext.getResources().getString(
+						R.string.no_role))
+						|| role.equals(mContext.getResources()
+								.getString(R.string.ambassadors).toLowerCase())) {
+					mViewHolder.txtRole.setText("");
 
-			}else{
-				mViewHolder.txtRole.setText(gridItemsList.get(position).getRole().trim());
+				} else {
+					mViewHolder.txtRole.setText(gridItemsList.get(position)
+							.getRole().trim());
+
+				}
+
+				mViewHolder.imgTeamMember
+						.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+				// mViewHolder.imgTeamMember.setPadding(5, 5, 5, 5);
+				convertView.setTag(mViewHolder);
+			} else {
+
+				convertView = inflater.inflate(R.layout.ambassador_item, null);
+				mViewHolder = new ViewHolder();
+				// mViewHolder.image = (ImageView)
+				// convertView.findViewById(R.id.image);
+				mViewHolder.imgTeamMember = (PhotoView) convertView
+						.findViewById(R.id.img_ambassador);
+				mViewHolder.txtPersonName = (TextView) convertView
+						.findViewById(R.id.txt_ambassador_name);
+				mViewHolder.errorTxt = (TextView) convertView
+						.findViewById(R.id.error_title);
+				// Util.setTextFont((Activity) mContext, mViewHolder.txtRole);
+				// Util.setTextFont((Activity) mContext,
+				// mViewHolder.txtPersonName);
+
+				mViewHolder.txtPersonName.setText(gridItemsList.get(position)
+						.getPersonName());
+
+				mViewHolder.imgTeamMember
+						.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+				// mViewHolder.imgTeamMember.setPadding(5, 5, 5, 5);
+				convertView.setTag(mViewHolder);
 
 			}
-
-			mViewHolder.imgTeamMember.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
-			// mViewHolder.imgTeamMember.setPadding(5, 5, 5, 5);
-			convertView.setTag(mViewHolder);
-		}
-		else {
+		} else {
 			mViewHolder = (ViewHolder) convertView.getTag();
 		}
-		mViewHolder.imgTeamMember.setLayoutParams(new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, reqImageHeight));
+		mViewHolder.imgTeamMember
+				.setLayoutParams(new RelativeLayout.LayoutParams(
+						LayoutParams.MATCH_PARENT, reqImageHeight));
 		mViewHolder.imgTeamMember.setScaleType(ImageView.ScaleType.MATRIX);
 
 		// Util.setTextFont((Activity) mcontext, mViewHolder.title);
-		mViewHolder.imgTeamMember.setImageDrawable(mContext.getResources().getDrawable(R.drawable.imagenotqueued));
-		mViewHolder.imgTeamMember.setTag(gridItemsList.get(position).getMemberThumbUrl());
+		mViewHolder.imgTeamMember.setImageDrawable(mContext.getResources()
+				.getDrawable(R.drawable.imagenotqueued));
+		mViewHolder.imgTeamMember.setTag(gridItemsList.get(position)
+				.getMemberThumbUrl());
 
 		if (isFrom.equals("team_members")) {
-			mViewHolder.imgTeamMember.setImageURL(gridItemsList.get(position).getMemberThumbUrl(), true, this.mEmptyDrawable, mViewHolder.errorTxt,false);
+			mViewHolder.imgTeamMember.setImageURL(gridItemsList.get(position)
+					.getMemberThumbUrl(), true, this.mEmptyDrawable,
+					mViewHolder.errorTxt, false);
 
-		}
-		else {
-			mViewHolder.imgTeamMember.setImageURL(gridItemsList.get(position).getMemberThumbUrl(), true, this.mEmptyDrawable, mViewHolder.errorTxt,false);
+		} else {
+			mViewHolder.imgTeamMember.setImageURL(gridItemsList.get(position)
+					.getMemberThumbUrl(), true, this.mEmptyDrawable,
+					mViewHolder.errorTxt, false);
 
 		}
 
@@ -126,7 +183,7 @@ public class TeamMemberGridAdapter extends BaseAdapter {
 
 	}
 
-	public void updateList (ArrayList <TeamMember> teamMembers) {
+	public void updateList(ArrayList<TeamMember> teamMembers) {
 		for (TeamMember teamMembers2 : teamMembers) {
 			gridItemsList.add(teamMembers2);
 		}
