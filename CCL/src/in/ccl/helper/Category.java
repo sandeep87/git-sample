dbe883c5ec3b68ba1c5b9c6a9d1c0091df0591c8
rajeshbabu <rajeshmca38@gmail.com>
2013-02-19 11:04:13 +0530

    deleting from master

--- CCL/src/in/ccl/helper/Category.java
+++
@@ -1,5 +0,0 @@
-package in.ccl.helper;
-
-public enum Category {
-	BANNER, VIDEO, PHOTO,FULL_SCREEN,TEAM_LOGO,TEAM_MEMBERS,TEAM_AMBASSADORS
-}
--- CCL/src/in/ccl/imageloader/FailToLoad.java
+++
@@ -1,9 +0,0 @@
-package in.ccl.imageloader;
-
-/**
- * Image loading failure reason.
- * */
-
-public enum FailToLoad {
-		IO_ERROR, OUT_OF_MEMORY, UNKNOWN	
-}
