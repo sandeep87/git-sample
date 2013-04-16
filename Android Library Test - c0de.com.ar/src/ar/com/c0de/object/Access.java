5158d77e2e743f1b59dac4bfe6a18646ef8d0cea
sandeep87 <sandeep.ayipilla@paradigmcreatives.com>
2013-04-16 16:07:41 +0530

    new projects

---
+++ Android Library Test - c0de.com.ar/src/ar/com/c0de/object/Access.java
@@ -0,0 +1,11 @@
+package ar.com.c0de.object;
+
+public class Access {
+
+	public int idSector;
+	
+	public Access() {
+		
+	} 
+	
+}
---
+++ Android Library Test - c0de.com.ar/src/ar/com/c0de/object/User.java
@@ -0,0 +1,16 @@
+package ar.com.c0de.object;
+
+public class User {
+
+	public String Name;
+	public String Pass;
+	
+	public Access accessm;
+	
+	public Access[] access;
+	
+	public User() {
+
+	}
+
+}
